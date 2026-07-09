37aba75746923040b1b594611420e619ce4833e6
Vinuta <vinutask0207.com>
2026-07-09 22:30:08 +0530

    Comitting unsaved files

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+}
+
+variable "bucket_name" {
+  description = "Unique S3 Bucket Name"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment Name"
+  type        = string
+}
